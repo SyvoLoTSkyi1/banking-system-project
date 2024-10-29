95378ef99648a96c09be2782dfb5f072b3250eeb
MykolaSyvolotskyi <nikolaysvl31@gmail.com>
2024-10-29 17:16:22 +0200

    create Account

---
+++ src/main/java/com/github/SyvoLoTSkyi1/bankingsystemproject/Account.java
@@ -0,0 +1,15 @@
+package com.github.SyvoLoTSkyi1.bankingsystemproject;
+
+public class Account {
+    String accountNumber;
+    double balance;
+
+    Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+
+    }
+
+
+}
+
--- src/main/java/com/github/SyvoLoTSkyi1/bankingsystemproject/BankingSystem.java
+++ src/main/java/com/github/SyvoLoTSkyi1/bankingsystemproject/BankingSystem.java
@@ -1,8 +1,17 @@
 package com.github.SyvoLoTSkyi1.bankingsystemproject;
 
+import java.util.Arrays;
+
 public class BankingSystem {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Banking System");
+
+        Account account = new Account("12345678", 1200);
+
+        System.out.println("Account number: " + account.accountNumber);
+        System.out.println("Account balance: " + account.balance);
+
+
     }
 }
