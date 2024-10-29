7df1873e3ac22e185d9f88b52a250a54370675dd
MykolaSyvolotskyi <nikolaysvl31@gmail.com>
2024-10-29 14:51:18 +0200

    project setup

---
+++ src/main/java/com/github/SyvoLoTSkyi1/bankingsystemproject/BankingSystem.java
@@ -0,0 +1,8 @@
+package com.github.SyvoLoTSkyi1.bankingsystemproject;
+
+public class BankingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Banking System");
+    }
+}
