4a32acbc4b6aa426201fdb0b6451d85a80bbefd2
MykolaSyvolotskyi <nikolaysvl31@gmail.com>
2025-02-12 13:23:27 +0200

    Add: create console ui
    - while loop
    
    - switch-case

--- src/main/java/com/github/SyvoLoTSkyi1/bankingsystemproject/BankingSystem.java
+++ src/main/java/com/github/SyvoLoTSkyi1/bankingsystemproject/BankingSystem.java
@@ -1,48 +1,45 @@
 package com.github.SyvoLoTSkyi1.bankingsystemproject;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class BankingSystem {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Banking System!");
 
-        Account account = new Account("12345678", "qwerty");
+        // New account creation
+        System.out.println("Create your account to start!");
+        System.out.print("Enter your account number: ");
+        String accountNumber = scanner.next();
+
+        Account account = new Account(accountNumber, "qwerty");
         System.out.println("The new account with account number " + account.getAccountNumber() + " was created!");
 
-        System.out.println("Account number: " + account.getAccountNumber());
-        System.out.println("Account balance: " + account.getBalance());
-        System.out.println("Account creation date: " + account.getCreationDate());
+        // Basic loop for users
+        while (true) {
+            System.out.println("\nChoose an option:");
+            System.out.println("1. Check balance");
+            System.out.println("2. Deposit money");
+            System.out.println("3. Withdraw money");
+            System.out.println("4. Exit");
 
-        double depositAmount = 1000;
-        String password1 = "qwerty";
-        if (account.checkPassword(password1)) {
-            boolean depositResult = account.deposit(depositAmount);
-            if (depositResult) {
-                System.out.println("Deposit of " + depositAmount + " amount to account was made successfully");
-            } else {
-                System.out.println("Deposit failed. Deposit amount must be bigger than zero");
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    //
+                case 2:
+                    //
+                case 3:
+                    //
+                case 4:
+                    //
+                default:
+                    System.out.println("Invalid option. Please enter a number from 1 to 4.");
             }
-            System.out.println("Account balance: " + account.getBalance());
-        } else {
-            System.out.println("Your password is incorrect");
         }
-
-
-        double withdrawalAmount = 300;
-        String password2 = "qwerty";
-        if (account.checkPassword(password2)) {
-            boolean withdrawalResult = account.withdrawal(withdrawalAmount);
-            if (withdrawalResult) {
-                System.out.println("Withdrawal of " + withdrawalAmount + " amount from account was made successfully");
-            } else {
-                System.out.println("Withdrawal failed. Withdrawal amount must be bigger than zero and equals or less than balance");
-            }
-            System.out.println("Account balance: " + account.getBalance());
-        } else {
-            System.out.println("Your password is incorrect");
-        }
-
 
     }
 }
