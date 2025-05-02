582ec6039dc54ba9cf6bd50dae81adb42febf2e4
MykolaSyvolotskyi <nikolaysvl31@gmail.com>
2025-05-02 14:21:14 +0300

    Refactor: Refactoring case 1, case 2 in while loop
    - create createAccount(), selectAccount(), manageAccount() in AccountService

--- src/main/java/com/github/SyvoLoTSkyi1/bankingsystemproject/AccountService.java
+++ src/main/java/com/github/SyvoLoTSkyi1/bankingsystemproject/AccountService.java
@@ -4,6 +4,67 @@ import java.util.List;
 
 public class AccountService {
 
+    public void createAccount(String accountNumber, String accountPassword, List<Account> accounts) {
+        Account newAccount = new Account(accountNumber, accountPassword);
+        System.out.println("\nThe new account with account number " + newAccount.getAccountNumber() + " was created!");
+        accounts.add(newAccount);
+    }
+
+    public Account selectAccount(String accountNumber, List<Account> accounts) {
+
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                return account;
+
+            }
+        }
+
+        return null;
+
+    }
+
+    public void manageAccount(InputHandler inputHandler, Account selectedAccount) {
+
+        while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Account information");
+            System.out.println("2. Check balance");
+            System.out.println("3. Deposit money");
+            System.out.println("4. Withdraw money");
+            System.out.println("5. Transaction history");
+            System.out.println("6. Go back\n");
+
+            int choice = inputHandler.getIntInput("\nYour choice: ");
+
+            switch (choice) {
+
+                case 1 -> getAccountInfo(selectedAccount);
+
+                case 2 -> getAccountBalance(selectedAccount);
+
+                case 3 -> {
+                    double depositAmount = inputHandler.getDoubleInput("\nEnter deposit amount: ");
+                    depositToAccount(selectedAccount, depositAmount);
+                }
+
+                case 4 -> {
+                    double withdrawalAmount = inputHandler.getDoubleInput("\nEnter withdrawal amount: ");
+                    withdrawalFromAccount(selectedAccount, withdrawalAmount);
+                }
+
+                case 5 -> accountTransactionHistory(selectedAccount);
+
+                case 6 -> {
+                    System.out.println("Back to Main Menu...");
+                    return;
+                }
+
+                default -> System.out.println("\nInvalid choice. Please enter a number from 1 to 6.");
+            }
+        }
+    }
+
+
     public void getAccountInfo(Account account) {
         System.out.println("\nAccount information:");
         System.out.println("Account number: " + account.getAccountNumber());
--- src/main/java/com/github/SyvoLoTSkyi1/bankingsystemproject/BankingSystem.java
+++ src/main/java/com/github/SyvoLoTSkyi1/bankingsystemproject/BankingSystem.java
@@ -10,9 +10,10 @@ public class BankingSystem {
         AccountService accountService = new AccountService();
         List<Account> accounts = new ArrayList<>();
 
+
         System.out.println("Welcome to the Banking System!");
 
-        outerLoop:
+
         while (true) {
             System.out.println("\nMain Menu:");
             System.out.println("1. Create a new account");
@@ -27,9 +28,8 @@ public class BankingSystem {
                     String accountNumber = inputHandler.getStringInput("\nEnter your account number: ");
                     String accountPassword = inputHandler.getStringInput("\nEnter your password: ");
 
-                    Account newAccount = new Account(accountNumber, accountPassword);
-                    System.out.println("\nThe new account with account number " + newAccount.getAccountNumber() + " was created!");
-                    accounts.add(newAccount);
+                    accountService.createAccount(accountNumber, accountPassword, accounts);
+
                 }
 
                 case 2 -> {
@@ -40,54 +40,11 @@ public class BankingSystem {
                         }
                         String accountNumber = inputHandler.getStringInput("\nEnter account number from the list: ");
 
-                        Account selectedAccount = null;
-
-                        for (Account account : accounts) {
-                            if (account.getAccountNumber().equals(accountNumber)) {
-                                selectedAccount = account;
-                                break;
-                            }
-                        }
+                        Account selectedAccount = accountService.selectAccount(accountNumber, accounts);
 
                         if (selectedAccount != null) {
 
-                            while (true) {
-                                System.out.println("\nMenu:");
-                                System.out.println("1. Account information");
-                                System.out.println("2. Check balance");
-                                System.out.println("3. Deposit money");
-                                System.out.println("4. Withdraw money");
-                                System.out.println("5. Transaction history");
-                                System.out.println("6. Go back\n");
-
-                                int choice = inputHandler.getIntInput("\nYour choice: ");
-
-                                switch (choice) {
-
-                                    case 1 -> accountService.getAccountInfo(selectedAccount);
-
-                                    case 2 -> accountService.getAccountBalance(selectedAccount);
-
-                                    case 3 -> {
-                                        double depositAmount = inputHandler.getDoubleInput("\nEnter deposit amount: ");
-                                        accountService.depositToAccount(selectedAccount, depositAmount);
-                                    }
-
-                                    case 4 -> {
-                                        double withdrawalAmount = inputHandler.getDoubleInput("\nEnter withdrawal amount: ");
-                                        accountService.withdrawalFromAccount(selectedAccount, withdrawalAmount);
-                                    }
-
-                                    case 5 -> accountService.accountTransactionHistory(selectedAccount);
-
-                                    case 6 -> {
-                                        System.out.println("Back to Main Menu...");
-                                        continue outerLoop;
-                                    }
-
-                                    default -> System.out.println("\nInvalid choice. Please enter a number from 1 to 6.");
-                                }
-                            }
+                            accountService.manageAccount(inputHandler, selectedAccount);
 
                         } else {
                             System.out.println("Invalid selection");
